aaa7a666d2e595cbb098d325070a5b8666ba2b02
AChung1020 <achun46@emory.edu>
2023-07-17 16:05:07 -0400

    new server credentials

--- src/server-AWS/UserPool.js
+++ src/server-AWS/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js"
 
 const poolData = {
-    UserPoolId: 'us-east-1_qSJHqyiAW',
-    ClientId: '6t41l0i94mnllbtc1ct3ps15bq'
+    UserPoolId: 'us-east-1_NJhknkhEI',
+    ClientId: '11b5lrm2o3cq430jfdag0gjq21'
 }
 
 export default new CognitoUserPool(poolData);
