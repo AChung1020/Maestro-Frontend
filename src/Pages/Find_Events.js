03da6e472efc569b1e9a517db8e5fd06c584acb4
JayPrasad1231 <jprasad1231@gmail.com>
2023-08-02 18:45:21 -0400

    Small changes to find_events

--- src/Pages/Find_Events.js
+++ src/Pages/Find_Events.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import { CognitoUser } from 'amazon-cognito-identity-js'
+import UserPool from '../server-AWS/UserPool'
 
 function Find_Events() {
     const [data, setData] = useState([]);
@@ -38,32 +40,63 @@ function Find_Events() {
     };
 
     return (
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ color: "black" }}>List of Events</h1>
             <center>
                 {data.map((dataObj, index) => (
-                    <div key={index}>
+                    <div key={index} style={{ marginBottom : '1rem'}}>
                         <Button onClick={() => handleShowModal(index)} variant="outline-primary">
                             {dataObj.eventDescription}
                         </Button>
                         <Modal show={showModals[index]} onHide={() => handleCloseModal(index)}>
                             <Modal.Header closeButton>
-                                <Modal.Title>{dataObj.eventDescription}</Modal.Title>
+                                <Modal.Title style={{ color: 'blue', fontSize: '24px' }}>{dataObj.eventDescription}</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
                                 <p>Cost: {dataObj.cost}</p>
                                 {/* Add other event information here */}
                             </Modal.Body>
                             <Modal.Footer>
+                                {/*<Button variant="secondary" onClick={(dataObj) => }*/}
                                 <Button variant="secondary" onClick={() => handleCloseModal(index)}>
                                     Close
                                 </Button>
                             </Modal.Footer>
                         </Modal>
                     </div>
+
                 ))}
             </center>
+            {/*{data.map((dataObj, index) => (*/}
+            {/*    <MyModal*/}
+            {/*        key={index}*/}
+            {/*        show={showModals[index]}*/}
+            {/*        onHide={() => handleCloseModal(index)}*/}
+            {/*        eventDescription={dataObj.eventDescription}*/}
+            {/*        cost={dataObj.cost}*/}
+            {/*    />*/}
+            {/*))}*/}
+
         </div>
+    );
+}
+
+function MyModal({ show, onHide, eventDescription, cost }) {
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>{eventDescription}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>Cost: {cost}</p>
+                {/* Add other event information here */}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
